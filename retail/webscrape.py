4145eaa73294d5d5ea762cc7a3c038a50b074541
hanivan <hanivan@uchicago.edu>
2025-02-19 10:33:16 -0600

    webscrape

--- retail/webscrape.py
+++ retail/webscrape.py
@@ -2,8 +2,29 @@ import time
 import httpx
 from urllib.parse import urlparse
 
-ALLOWED_DOMAINS = ("https://scrapple.fly.dev",)
+link1 = "https://www.propertyshark.com/cre/retail/us/il/cook-county/60022/"
+link2 = "https://www.propertyshark.com/cre/retail/us/il/cook-county/60022/?IncludeCoworking=false&CoworkingWorkspaceTypes=0&MapView=True&Zoom=14&Viewport=-87.78428869067383,42.1321636311465,-87.75733785449219,42.17441444701207&GeopickerType=viewport"
 REQUEST_DELAY = 0.1
+
+headers = {
+    "authority": "www.propertyshark.com",
+    "method": "GET",
+    "scheme": "https",
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "accept-encoding": "gzip, deflate, br, zstd",
+    "accept-language": "en-US,en;q=0.9",
+    "cache-control": "max-age=0",
+    "priority": "u=0, i",
+    "sec-ch-ua": '"Not(A:Brand";v="99", "Google Chrome";v="133", "Chromium";v="133")',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"macOS"',
+    "sec-fetch-dest": "document",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-site": "same-origin",
+    "sec-fetch-user": "?1",
+    "upgrade-insecure-requests": "1",
+    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36"
+}
 
 
 def make_request(url):
@@ -19,26 +40,3 @@ def make_request(url):
     resp = httpx.get(url)
     resp.raise_for_status()
     return resp
-
-
-def make_link_absolute(rel_url, current_url):
-    """
-    Given a relative URL like "/abc/def" or "?page=2"
-    and a complete URL like "https://example.com/1/2/3" this function will
-    combine the two yielding a URL like "https://example.com/abc/def"
-
-    Parameters:
-        * rel_url:      a URL or fragment
-        * current_url:  a complete URL used to make the request that
-                        contained a link to rel_url
-
-    Returns:
-        A full URL with protocol & domain that refers to rel_url.
-    """
-    url = urlparse(current_url)
-    if rel_url.startswith("/"):
-        return f"{url.scheme}://{url.netloc}{rel_url}"
-    elif rel_url.startswith("?"):
-        return f"{url.scheme}://{url.netloc}{url.path}{rel_url}"
-    else:
-        return rel_url
