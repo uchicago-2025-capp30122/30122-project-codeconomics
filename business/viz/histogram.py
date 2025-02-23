b35fb945a322e0973554c4ba682a38a4cda7a66d
hanivan <hanivan@uchicago.edu>
2025-02-23 01:43:57 -0600

    Histogram

---
+++ business/viz/histogram.py
@@ -0,0 +1,12 @@
+import plotly.express as px
+import csv
+import pandas as pd
+import pathlib
+
+path_income_csv = pathlib.Path(__file__).parent.parent / 'data/median_income.csv'
+data_income = pd.read_csv(path_income_csv)
+
+def make_hist(series, col):
+    # Create Histogram
+    fig = px.histogram(series, x=col, title=f'Histogram of {col}')
+    fig.show()
