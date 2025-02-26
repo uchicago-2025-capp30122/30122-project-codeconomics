6953d147d1f980518f57ddb67a7e9e9a8ae4d873
hanivan <hanivan@uchicago.edu>
2025-02-26 17:32:57 -0600

    initialize business outlook

---
+++ business/analyze/business_outlook.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from pathlib import Path
+
+# define data path
+data_path = Path(__file__).parent.parent / "data"
+
+def new_business():
+    """
+    get a table of new business occurrence
+    """
+
+    
+
+    
