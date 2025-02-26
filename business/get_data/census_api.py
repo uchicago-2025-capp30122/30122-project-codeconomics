f63d2652df9e9743a6441d1a03909f3a0ba93fae
hanivan <hanivan@uchicago.edu>
2025-02-26 16:18:40 -0600

    adding population size to median_income.csv

--- business/get_data/census_api.py
+++ business/get_data/census_api.py
@@ -1,8 +1,10 @@
 import httpx
 import csv
+import pathlib
 from .utils import ZIP_CODES
 
-url = 'https://api.census.gov/data/2023/acs/acs5?get=B06011_001E&for=zip%20code%20tabulation%20area:*&key='    
+url = 'https://api.census.gov/data/2023/acs/acs5?get=B06011_001E,B01003_001E&for=zip%20code%20tabulation%20area:*&key='
+path_data = pathlib.Path(__file__).parent.parent / 'data' 
 
 def get_census_data(api_key):
     """
@@ -15,8 +17,8 @@ def get_census_data(api_key):
     resp = httpx.get(census_link)
     resp.raise_for_status()
 
-    with open('business/data/median_income.csv', 'w') as file:
+    with open(path_data / 'median_income.csv', 'w') as file:
         writer = csv.writer(file)
-        writer.writerow(['med_income','zip_code'])
-        filtered_rows = (row for row in resp.json()[1:] if row[1] in ZIP_CODES)
+        writer.writerow(['med_income','population_size','zip_code'])
+        filtered_rows = (row for row in resp.json()[1:] if row[2] in ZIP_CODES)
         writer.writerows(filtered_rows)
